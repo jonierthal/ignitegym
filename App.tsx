8462b10b4008de7a761ef206a895a46add18c51d
Jonathan Erthal <jonierthal@gmail.com>
2023-03-07 16:35:42 -0300

    primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'; 
+
+import {} from '@
 
 export default function App() {
   return (
