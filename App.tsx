b480d7409f360f9d60c792fa084a133f3935cd17
Jonathan Erthal <jonierthal@gmail.com>
2023-03-09 07:55:10 -0300

    Utilizando o NativeBase

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
-import {  Text, View, StatusBar } from 'react-native'; 
+import { View, StatusBar } from 'react-native'; 
 
 import { NativeBaseProvider } from 'native-base';
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
 
 
 export default function App() {
@@ -15,7 +17,8 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <View/> : <View/>}
+
+       <Loading />
     </NativeBaseProvider>
   );
 }
