beaeb2e9b4d3ad5454ed8cca0fcff34b5b406835
Jonathan Erthal <jonierthal@gmail.com>
2023-03-20 07:58:55 -0300

    Aplicando navegação de autenticação

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,10 +3,20 @@ import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
+import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
+
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { useNavigation } from '@react-navigation/native';
 
 export function SignIn() {
+
+    const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+    function handleNewAccount() {
+        navigation.navigate('singUp');
+    }
+
     return (
         <ScrollView contentContainerStyle={{ flexGrow: 1}} showsVerticalScrollIndicator={false}>
             <VStack 
@@ -61,6 +71,7 @@ export function SignIn() {
                     <Button 
                         title="Criar Conta" 
                         variant="outline"
+                        onPress={handleNewAccount}
                     />
                 </Center>
 
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,4 +1,5 @@
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import { useNavigation } from '@react-navigation/native';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -7,6 +8,13 @@ import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 export function SignUp() {
+
+    const navigation = useNavigation();
+
+    function handleGoBack() {
+        navigation.goBack();
+    }
+
     return (
         <ScrollView contentContainerStyle={{ flexGrow: 1}} showsVerticalScrollIndicator={false}>
             <VStack 
@@ -57,6 +65,7 @@ export function SignUp() {
                         title="Voltar para o login" 
                         variant="outline"
                         mt={24}
+                        onPress={handleGoBack}
                     />
 
             </VStack>
