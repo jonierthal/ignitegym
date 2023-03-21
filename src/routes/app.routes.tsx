441030950267557aca19642a4a7cb1f3fcee7753
Jonathan Erthal <jonierthal@gmail.com>
2023-03-21 07:50:19 -0300

    Removendo o Header

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,16 +1,26 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 
 import { Home } from '@screens/Home';
 import { Exercise } from '@screens/Exercise';
 import { History } from '@screens/History';
 import { Profile } from '@screens/Profile';
 
+type AppRoutes = {
+    home: undefined;
+    exercise: undefined;
+    profile: undefined;
+    history: undefined;
+}
 
-const {Navigator,Screen}= createBottomTabNavigator();
+const {Navigator,Screen}= createBottomTabNavigator<AppRoutes>();
+
+export type AppNavgatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
 
 export function AppRoutes() {
     return (
-        <Navigator>
+        <Navigator screenOptions={{
+            headerShown: false,
+        }}>
             <Screen
                 name="home"
                 component={Home}
