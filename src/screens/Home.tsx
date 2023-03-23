896ba4777b39adffe258b6a922c86feecf746bfb
Jonathan Erthal <jonierthal@gmail.com>
2023-03-23 07:42:37 -0300

    Estilizando evento de Pressed

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,17 @@
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import { VStack, Text } from 'native-base';
+import { VStack, Text, HStack } from 'native-base';
 
 export function Home(){
     return (
         <VStack flex={1}>
             <HomeHeader />
+            
+            <HStack>
+                <Group name="costas"/>
+                <Group name="ombro"/>
 
-            <Group name="costas"/>
+            </HStack>
         </VStack>
     );
 }
