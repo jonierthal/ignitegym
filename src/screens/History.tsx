f6a1ac915c5be13d1fc734e403f721e8ef14fc3d
Jonathan Erthal <jonierthal@gmail.com>
2023-03-27 07:53:22 -0300

    Componente HistoryCard

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,12 +1,16 @@
 import { VStack } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
+import { HistoryCard } from '@components/HistoryCard';
 
 
 export function History(){
     return (
         <VStack flex={1}>
             <ScreenHeader title="Histórico de exercícios"/>
+            <HistoryCard/>
+            <HistoryCard/>
+            <HistoryCard/>
         </VStack>
     );
 }
