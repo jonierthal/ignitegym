0f56d9a026966aa1ac2befc6d30a5ea139c7d50e
Jonathan Erthal <jonierthal@gmail.com>
2023-03-28 07:46:33 -0300

    Mensagem de lista vazia

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,4 +1,4 @@
-import { Heading, VStack, SectionList } from 'native-base';
+import { Heading, VStack, SectionList, Text } from 'native-base';
 import { useState } from 'react';
 
 import { ScreenHeader } from '@components/ScreenHeader';
@@ -32,6 +32,13 @@ export function History(){
                     </Heading>
                 )}
                 px={8}
+                contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center'}}
+                ListEmptyComponent={() => (
+                    <Text color="gray.100" textAlign="center">
+                        Não há exercícios registrados ainda.{'\n'}
+                        Vamos fazer exercícios hoje?
+                    </Text>
+                )}
             />
         </VStack>
     );
