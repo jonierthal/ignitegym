a6ee96465e4bc9b4721a0051f873a5731806756d
Jonathan Erthal <jonierthal@gmail.com>
2023-04-04 08:12:53 -0300

    Utilizando mensagens toast

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -5,15 +5,17 @@ import { UserPhoto } from '@components/UserPhoto';
 
 import * as FileSystem from 'expo-file-system';
 import * as ImagePicker from 'expo-image-picker';
-import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from 'native-base';
 import { useState } from 'react';
-import { Alert, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
 const PHOTO_SIZE = 33;
 
 export function Profile(){
     const [photoIsLoading, setPhotoIsLoading] = useState(false);
     const [userPhoto, setUserPhoto] = useState('https://github.com/jonierthal.png');
+
+    const toast = useToast();
 
     async function  handleUserPhotoSelect(){
         setPhotoIsLoading(true);
@@ -34,8 +36,13 @@ export function Profile(){
                     const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
                     
                     if(photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
-                        return Alert.alert("Essa imagem é muito grande. Escolha uma de até 5MB");
+                        return toast.show({
+                            title: 'Essa imagem é muito grande. Escolha uma de até 5MB',
+                            placement: 'top',
+                            bgColor: 'red.500'
+                        });
                     }
+
                     setUserPhoto(photoSelected.assets[0].uri);
                 }
         
