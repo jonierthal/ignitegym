d431a61a49e15075e6f7f70783a76c23f0652057
Jonathan - TI | PCR <jonathan.erthal@pcr.local>
2023-07-14 08:09:47 -0300

    Tratando Erros da Api

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,18 +1,17 @@
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from 'native-base';
 import { useNavigation } from '@react-navigation/native'
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
 import { api } from '@services/api';
-import axios from 'axios';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
-import { Alert } from 'react-native';
+import { AppError } from '@utils/AppError';
 
 type FormDataProps = {
     name: string;
@@ -29,6 +28,9 @@ const singUpSchema = yup.object({
 });
 
 export function SignUp() {
+
+    const toast = useToast();
+
     const { control, handleSubmit, formState: { errors} } = useForm<FormDataProps>({
         resolver: yupResolver(singUpSchema)
     });
@@ -45,9 +47,14 @@ export function SignUp() {
             const response = await api.post('/users', { name, email, password });
             console.log(response.data);
         }catch(error){
-            if(axios.isAxiosError(error)){
-                Alert.alert(error.response?.data.message);
-            }
+            const isAppError = error instanceof AppError;
+            const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde.';
+
+            toast.show({
+                title,
+                placement: 'top',
+                bgColor: 'red.500'
+            });
         }
     }
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,16 @@
+import { AppError } from '@utils/AppError';
 import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'http://192.168.0.54:3333'
 });
 
-api.interceptors.response.use((response) => {
-    console.log("INTERCEPTOR RESPONSE =>", response);
-    return response;
-}, (error) => {
-    console.log("INTERCEPTOR RESPONSE ERROR =>", error);
-    return Promise.reject(error);
-})
+api.interceptors.response.use(response => response, error => {
+    if(error.response && error.response.data){
+        return Promise.reject(new AppError(error.response.data.message));
+    } else {
+        return Promise.reject(error);
+    }
+});
 
 export { api };
