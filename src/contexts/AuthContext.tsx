4bbaded21654e7d8bbdb40ffe1ffccb25d495a4b
Jonathan Erthal - TI | PCR <jonathan.erthal@pcr.ind.br>
2023-08-17 07:48:37 -0300

    Carregando usuário logado

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { UserDTO } from "@dtos/UserDTO";
 
-import { storageUserSave } from '@storage/storageUser';
+import { storageUserSave, storageUserGet } from '@storage/storageUser';
 
 import { api } from "@services/api";
 
@@ -31,6 +31,18 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
             throw error;
         }
     }
+
+    async function loadUserData() {
+        const userLogged = await storageUserGet();
+
+        if(userLogged){
+            setUser(userLogged);
+        }
+    }
+
+    useEffect(() => {
+        loadUserData();
+    },[]);
 
     return (
         <AuthContext.Provider value={{ user, signIn }}>
