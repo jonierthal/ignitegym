4abd325eeec812a6430727e027ceb4efc99b308c
Jonathan Erthal - TI | PCR <jonathan.erthal@pcr.ind.br>
2023-08-18 07:55:34 -0300

    Criando conta e logando usuário

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -12,6 +12,8 @@ import BackgroundImg from '@assets/background.png';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import { AppError } from '@utils/AppError';
+import { useState } from 'react';
+import { useAuth } from '@hooks/useAuth';
 
 type FormDataProps = {
     name: string;
@@ -29,7 +31,10 @@ const singUpSchema = yup.object({
 
 export function SignUp() {
 
+    const [isLoading, setIsLoading] = useState(false);
+
     const toast = useToast();
+    const { signIn } = useAuth();
 
     const { control, handleSubmit, formState: { errors} } = useForm<FormDataProps>({
         resolver: yupResolver(singUpSchema)
@@ -44,9 +49,14 @@ export function SignUp() {
     async function handleSignUp({ name, email, password, password_confirm}: FormDataProps) {
 
         try{
-            const response = await api.post('/users', { name, email, password });
-            console.log(response.data);
+            setIsLoading(true);
+
+            await api.post('/users', { name, email, password });
+            await signIn(email, password);
+
         }catch(error){
+            setIsLoading(false);
+
             const isAppError = error instanceof AppError;
             const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde.';
 
@@ -148,6 +158,7 @@ export function SignUp() {
                     <Button 
                         title="Criar e acessar" 
                         onPress={handleSubmit(handleSignUp)}
+                        isLoading={isLoading}
                     />
 
                 </Center>
