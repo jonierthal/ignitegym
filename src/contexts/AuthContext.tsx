fa041668f86617389f410214f4b689ae2f0e9b5a
Jonathan Erthal - TI | PCR <jonathan.erthal@pcr.ind.br>
2023-08-22 07:54:59 -0300

    Removendo o token do storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,7 +2,7 @@ import { createContext, ReactNode, useEffect, useState } from "react";
 import { UserDTO } from "@dtos/UserDTO";
 
 import { storageUserSave, storageUserGet, storageUserRemove } from '@storage/storageUser';
-import { storageAuthTokenSave, storageAuthTokenGet } from "@storage/storageAuthToken";
+import { storageAuthTokenSave, storageAuthTokenGet, storageAuthTokenRemove } from "@storage/storageAuthToken";
 
 import { api } from "@services/api";
 
@@ -63,6 +63,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
             setUser({} as UserDTO);
             await storageUserRemove();
+            await storageAuthTokenRemove();
 
         } catch (error) {
             throw error;
